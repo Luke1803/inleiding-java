1e72e56f7bda9de9468289820b2077e0998ddb42
Luke1803 <lkogenhop@gmail.com>
2019-09-06 08:59:03 +0200

    driehoek

--- src/h04/JavaTekenen.java
+++ src/h04/JavaTekenen.java
@@ -15,10 +15,5 @@ public class JavaTekenen extends Applet{
         g.drawLine(40, 160, 90, 40);
         g.drawLine(90, 40, 140, 160);
         g.setColor(Color.red);
-        base = 200;
-        height = 220;
-        g.drawLine(100, 200,100+base,200);
-        g.drawLine(100,200,100+base/2,height);
-        g.drawLine(100+base,200,100+base/2,height);
     }
 }
