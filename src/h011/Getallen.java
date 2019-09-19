fe372876046f002225d3e4e86f6cb82f0dd8bcea
Luke1803 <lkogenhop@gmail.com>
2019-09-19 11:12:20 +0200

    Opdracht2

---
+++ src/h011/Getallen.java
@@ -0,0 +1,21 @@
+package h011;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Getallen extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller = 10;
+        int y = 0;
+
+        while(teller < 20) {
+            y += 20;
+            g.drawString("" + teller, 305, y );
+            teller++;
+        }
+    }
+}
