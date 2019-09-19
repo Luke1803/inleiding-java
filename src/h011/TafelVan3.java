628f6d64a6d445e68b1e9c3a2b298e64f9f9bb5d
Luke1803 <lkogenhop@gmail.com>
2019-09-19 13:10:00 +0200

    Opdracht4

---
+++ src/h011/TafelVan3.java
@@ -0,0 +1,28 @@
+package h011;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class TafelVan3 extends Applet {
+
+    public void init() {
+
+
+    }
+
+
+    public void paint(Graphics g) {
+        int uitkomst;
+        int cijfer = 1;
+        int teller;
+        int y = 20;
+        for (teller = 0;teller  <10 ; teller++) {
+            uitkomst = cijfer * 3;
+            g.drawString("" + uitkomst,20,y);
+            cijfer++;
+            y += 20;
+            
+        }
+
+    }
+}
